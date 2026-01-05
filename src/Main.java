1ae6c4f56b6b0c9f038500482e976a3042266de2
sadun <sadunmith3@gmail.com>
2026-01-05 22:09:21 +0530

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+import view.MainFrame;
 
+public class Main {
+    public static void main(String[] args) {
+        new MainFrame();
+    }
 }
